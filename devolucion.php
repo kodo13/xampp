d7e378f6986a67644ac98af3409c4334826b4788
Ionut Birsanu <ibirsanu023@gmail.com>
2023-01-31 16:58:08 +0100

    vehiculo devolto a taboa de devoltos, falta aprobacion por admin para pasar a vehiculo en aluguer

--- devolucion.php
+++ devolucion.php
@@ -21,7 +21,7 @@ $user= $_SESSION['usuario'];
 
 #Hacemos la comprobación de si el usuario tiene la sesión iniciada, si no, redirige a que lo haga.
 
-#Si lo está, saltamos al menú de vehículos.
+#Si lo está, mostramos vehículos a devolver.
 
 if(!isset($_SESSION["usuario"])){
     #Si entra aquí, no tiene sesión inciciada y mandamos a login.
@@ -38,13 +38,66 @@ if(!isset($_SESSION["usuario"])){
 
     
     if (isset($_REQUEST['devolucion'])) {
+        
+        #Restamos 1 á cantidade do vehiculo que ten ese usuario e ese modelo.
 
         $update2 = "UPDATE vehiculo_alugado SET cantidade=cantidade -1 WHERE modelo='$modelo' and usuario='$user'";
         $result_update2 = mysqli_query($mysqli_link, $update2);
 
-        echo "Vehículo devolto con éxito!!</br>";
-        echo "Volvendo ao menú do usuario... ";
-        header("refresh: 5; url = menu_user_form.php");
+        #Ejecutamos consulta á táboa vehiculo_devolto para ver se o usuario xa devolveu ese modelo. 
+        #Si ten o mesmo modelo devolto, sumamos un, se non ten, facemos inserción na táboa.
+
+        $select_dev= "SELECT * from vehiculo_devolto where modelo='$modelo' and usuario='$user'";
+        $result_dev = mysqli_query($mysqli_link, $select_dev);
+        $num_filas_ss=$result_dev->num_rows; 
+        
+        #Comprobamos si la consulta devuelve algun resultado
+        if ($num_filas_ss > 0 ){
+    
+            $update_devolto = "UPDATE vehiculo_devolto SET cantidade=cantidade +1 WHERE modelo='$modelo' and usuario='$user'";
+            $result_update_devolto = mysqli_query($mysqli_link, $update_devolto);
+
+            echo "Vehículo devolto con éxito! </br>";
+            echo "Volvendo ao menú do usuario... </br>";
+            header("refresh: 5; url = menu_user_form.php");
+            
+        }
+        else{
+            $select_alugado = "SELECT * FROM vehiculo_alugado WHERE modelo='$modelo' and usuario='$user'";
+            $result_alugado = mysqli_query($mysqli_link, $select_alugado);
+
+            $fila = mysqli_fetch_array($result_alugado, MYSQLI_ASSOC);
+
+            #Recollemos os datos do modelo que o usuario quere devolver.
+        
+            $cantidade = $fila['cantidade'];
+            $descricion = $fila['descricion'];
+            $marca = $fila['marca'];
+            $foto = $fila['foto'];
+            #Hacemos insert de los datos del vehicuo devolto con 1 cantidad.
+            $insert = "INSERT INTO vehiculo_devolto (modelo, cantidade, descricion, marca, foto, usuario) 
+                VALUES ('$modelo',1,'$descricion','$marca','$foto','$user')";
+
+            $result_insert = mysqli_query($mysqli_link, $insert);
+
+            if( $result_insert){ #Si verdadero, entonces update hecho correctamente.
+                echo "Vehículo devolto con éxito! </br> ";
+                echo "Volvendo ao menú do usuario... </br>";
+                header("refresh: 5; url = menu_user_form.php");
+
+            } else {
+                echo "Oubo un erro fatal ao devolver o vehículo!!" . mysqli_error($mysqli_link);
+
+
+            }   
+
+                
+        }
+
+
+        #Eliminamos os datos dos vehiculos dos que xa non quedan cantidades a devolver.
+        $delete0 = "DELETE FROM vehiculo_alugado WHERE cantidade=0";
+        $result_delete0 = mysqli_query($mysqli_link, $delete0);
 
     }
 }
